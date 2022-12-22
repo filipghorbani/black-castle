fa142038c84e9e9d5b5f0e980d38f1712a5960c5
Filip Ghorbani <filip.ghorbani@consid.se>
2022-12-22 22:50:20 +0100

    Adding firestore

--- src/components/Pizza/index.jsx
+++ src/components/Pizza/index.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import facebook from "../../images/facebook.svg";
 import pin from "../../images/pin.svg";
 import instagram from "../../images/instagram.svg";
 import "./Pizza.css";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
+import { db } from "../../firebase.js";
+import { collection, getDocs, listCollections } from "firebase/firestore"; 
 
 const Pizza = () => {
+
+  const [groups, setGroups] = useState([])
+  const [pizzor, setPizzor] = useState([])
+  
+  useEffect(() => {
+    const getData = async() => {
+      const data1 = await getDocs(collection(db, "Groups"));
+      setGroups(data1.docs.map((doc) => ({...doc.data(), id: doc.id})))
+
+      const data2 = await getDocs(collection(db, "Pizzas"));
+      setPizzor(data2.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      // //   querySnapshot2.forEach((doc) => {
+      // //   const data = doc.data()
+      // //   setPizzor([...pizzor, Pizza(data["Name"], data["Desc"], group.name)])  
+      // // });)
+    }
+    getData()
+  }, [])
+
+
   return (
     <div id="top" className="Pizza">
       <div className="head">
@@ -37,193 +59,27 @@ const Pizza = () => {
       </div>
       <div className="title">- Pizzor -</div>
       <div className="items">
-        <div className="group">
-          <h3>Grupp 1</h3>
-          <p>Barnpizza 75 kr | Avhämtning 85 kr | Servering 95 kr</p>
-        </div>
-        <div className="container">
-          <div className="item">
-            <h4>- Margarita -</h4>
-            <p>Tomat och ost</p>
-          </div>
-          <div className="item">
-            <h4>- Fungi -</h4>
-            <p>Tomat, ost och champinjoner</p>
-          </div>
-          <div className="item">
-            <h4>- Veusivo -</h4>
-            <p>Tomat, ost och skinka</p>
-          </div>
-          <div className="item">
-            <h4>- Calzone -</h4>
-            <p>Tomat, ost och skinka (inbakad)</p>
-          </div>
-          <div className="item">
-            <h4>- Du Chef -</h4>
-            <p>Tomat, ost och köttfärs</p>
-          </div>
-          <div className="item">
-            <h4>- Capricciosa -</h4>
-            <p>Tomat, ost, skinka och färska champinjoner</p>
-          </div>
-          <div className="item">
-            <h4>- Hawaii -</h4>
-            <p>Tomat, ost, skinka och ananas</p>
-          </div>
-        </div>
-        <div className="group">
-          <h3>Grupp 2</h3>
-          <p>Barnpizza 85 kr | Avhämtning 90 kr | Servering 100 kr</p>
-        </div>
-        <div className="container">
-          <div className="item">
-            <h4>- Bussola -</h4>
-            <p>Tomat, ost, skinka och räkor</p>
-          </div>
-          <div className="item">
-            <h4>- Marinara -</h4>
-            <p>Tomat, ost, räkor och musslor</p>
-          </div>
-          <div className="item">
-            <h4>- Al Tonno -</h4>
-            <p>Tomat, ost, lök och tonfisk</p>
-          </div>
-          <div className="item">
-            <h4>- Toscana -</h4>
-            <p>Tomat, ost, lök, ägg och bacon</p>
-          </div>
-          <div className="item">
-            <h4>- Campaniola -</h4>
-            <p>Tomat, ost, lök, oliver, och salami</p>
-          </div>
-        </div>
-        <div className="group">
-          <h3>Grupp 3</h3>
-          <p>Barnpizza 90 kr | Avhämtning 95 kr | Servering 105 kr</p>
-        </div>
-        <div className="container">
-          <div className="item">
-            <h4>- Vegetale -</h4>
-            <p>
-              Tomat, ost, rödlök, färska champinjoner, paprika, kronärtskocka,
-              oliver och ruccola
-            </p>
-          </div>
-          <div className="item">
-            <h4>- Napoletana -</h4>
-            <p>Tomat, ost, oliver, kapris och sardeller</p>
-          </div>
-          <div className="item">
-            <h4>- Orientale special -</h4>
-            <p>Tomat, ost, champinjoner, ägg, lök och köttfärs</p>
-          </div>
-          <div className="item">
-            <h4>- Mamma Mia -</h4>
-            <p>Tomat, ost, lök, färska tomater, bacon, salami och köttfärs</p>
-          </div>
-          <div className="item">
-            <h4>- Bolognese -</h4>
-            <p>Tomat, ost, purjolök, fetaost, oliver och köttfärs</p>
-          </div>
-          <div className="item">
-            <h4>- Calzone special -</h4>
-            <p>Tomat, ost, skinka, färska champinjoner och räkor</p>
-          </div>
-          <div className="item">
-            <h4>- Tropicana -</h4>
-            <p>Tomat, ost, skinka, banan, jordnötter och curry</p>
-          </div>
-          <div className="item">
-            <h4>- Kebab Pizza -</h4>
-            <p>
-              Tomat, ost, lök, pepperoni, färska tomater, kebabkött och
-              vitlökssås
-            </p>
-          </div>
-        </div>
-        <div className="group">
-          <h3>Grupp 4</h3>
-          <p>Barnpizza 95 kr | Avhämtning 100 kr | Servering 110 kr</p>
-        </div>
-        <div className="container">
-          <div className="item">
-            <h4>- Quattro Stagioni -</h4>
-            <p>
-              Tomat, ost, skinka, färska champinjoner, räkor, kronärtskocka och
-              oliver
-            </p>
-          </div>
-          <div className="item">
-            <h4>- Quatro Formaggio -</h4>
-            <p>Tomat, ost, mozzarella, gorgonzola, fetaost och parmesanost</p>
-          </div>
-          <div className="item">
-            <h4>- Casabella -</h4>
-            <p>Tomat, ost, färska champinjoner, räkor, tonfisk och curry</p>
-          </div>
-          <div className="item">
-            <h4>- El Pollo -</h4>
-            <p>
-              Tomat, ost, paprika, ruccola, rödlök, färska tomater, kyckling och
-              curry
-            </p>
-          </div>
-          <div className="item">
-            <h4>- Messicana -</h4>
-            <p>Tomat, mozzarella, rödlök, vitlök, jalapeños och köttfärs</p>
-          </div>
-        </div>
-        <div className="group">
-          <h3>Grupp 5</h3>
-          <p>Barnpizza 110 kr | Avhämtning 115 kr |Servering 125 kr</p>
-        </div>
-        <div className="container">
-          <div className="item">
-            <h4>- El Toro -</h4>
-            <p>
-              Tomat, ost, skinka, färska champinjoner och räkor (dubbelinbakad)
-            </p>
-          </div>
-          <div className="item">
-            <h4>- Ciao Ciao -</h4>
-            <p>
-              Tomat, ost, färska tomater, lök, färska champinjoner, oxfilé och
-              bearnaisesås (halvinbakad)
-            </p>
-          </div>
-          <div className="item">
-            <h4>- Delicate -</h4>
-            <p>
-              Tomat, mozzarellaost, paprika, vitlök, jalapeños, oxfilé och
-              bearnaisesås
-            </p>
-          </div>
-          <div className="item">
-            <h4>- Gorgonzola -</h4>
-            <p>
-              Tomat, ost, lök, färska tomater, fläskfilé och gorgonzolaost
-              (halvinbakad)
-            </p>
-          </div>
-          <div className="item">
-            <h4>- Roma -</h4>
-            <p>Tomat, mozzarella, parmaskinka, ruccola och riven parmesan</p>
-          </div>
-          <div className="item">
-            <h4>- Bella Rasbo -</h4>
-            <p>
-              Tomat, ost, lök, färska champinjoner, salami, paprika, färska
-              tomater, bacon och köttfärs
-            </p>
-          </div>
-          <div className="item">
-            <h4>- Trävlinge special -</h4>
-            <p>
-              Tomat, ost, lök, pommes frites, pepperoni, isbergssallad, kebab
-              och vitlökssås
-            </p>
-          </div>
-        </div>
+        {/* {console.log(groups)} */}
+        {/* {console.log(pizzor)} */}
+          {groups.map((group) => (
+            <>
+              <h3>Group {group.id}</h3>
+              <p>Barnpizza {group.Children} kr | Avhämtning {group.Pickup} kr | Servering {group.Serve} kr</p>
+              <div className="container">
+                {pizzor.map((pizza) => (
+                  <>
+                    {pizza.Group == group.id ? 
+                    <div className="item">
+                      <h4>- {pizza.id} -</h4>
+                      <p>{pizza.Desc}</p>
+                    </div> : null}
+                  </>
+                ))
+              }
+              </div>
+            </>
+          ))
+        }
       </div>
       <div className="footer">
         <div className="small">
